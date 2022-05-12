737a2e4a6e8918853315f1175b831ec3bc19f94c
Abdy <abdyrush@gmail.com>
2022-05-12 21:49:32 +0200

    updated things

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Learning-C
 A simple italian repo to learn or to review C language
-https://www.w3schools.com/c/c_variables.php
+https://www.w3schools.com/c/c_data_types.php
--- main.c
+++ main.c
@@ -14,6 +14,11 @@ int main() {
 
 
     //! variabili
+    //* regole variabili:
+    //* 1) devono iniziare con una lettera o un underscore(_)
+    //* 2) sono case sensitive
+    //* 3) non possono contenere spazi oppure simboli strani (!, #, $, %, &, * ecc...)
+   
     // * formattazione: tipo nomeVar = valore;  esempio: int a = 1;
 
     int numero = 15; //? crea una variabile di tipo integer chiamata "numero" di cui valore 15
@@ -29,9 +34,33 @@ int main() {
     float varDecimale = 1.10; //? crea una variabile di tipo float chiamata "varDecimale" di cui valore 1.10
     char varCarattere = 'M'; //? crea una variabile di tipo char chiamata "varCarattere" di cui valore 'a'
 
-    printf("\n\n%d", varIntera); //? stampa il valore di "varIntera"
-    printf("\n%f", varDecimale); //? stampa il valore di "varDecimale"
-    printf("\n%c", varCarattere); //? stampa il valore di "varCarattere"
+    printf("\n\n %d", varIntera); //? stampa il valore di "varIntera"
+    printf("\n %f", varDecimale); //? stampa il valore di "varDecimale"
+    printf("\n %c", varCarattere); //? stampa il valore di "varCarattere"
     
+    //? più var in un print
+
+    printf("\n\n numero:%d\n Decimale:%f\n Char:%c", varIntera, varDecimale, varCarattere); //? stampa il valore di "varIntera", "varDecimale" e "varCarattere"
+
+    //? somma di due Var
+    int varSomma = varIntera + numero; //? crea una variabile di tipo integer chiamata "varSomma" di cui valore 20
+    printf("\n\n Somma:%d", varSomma); //? stampa il valore di "varSomma"
+
+    //? dichiarare più variabili contemporaneamente
+    int var1 = 5, var2 = 10, var3 = 15; //? crea tre variabili di tipo integer
+    printf("\n\n %d", var1 + var2 + var3); //? stampa il valore di "var1" sommato a quello di "var2" sommato a quello di "var3"
+
+    //? incrementare il valore di una variabile int di 1
+    var1++; //? incrementa il valore di "var1" di 1
+    printf("\n\n %d", var1); //? stampa il valore di "var1"
+
+    //? incrementare il valore di una variabile di più di 1
+    var1 += 2; //? incrementa il valore di "var1" di 2
+    printf("\n\n %d", var1); //? stampa il valore di "var1"
+
+
+    //! Tipi di dati
+    
+
     return 0; //? restituisce 0, perchè il programma è terminato correttamente
 }
