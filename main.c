9064a4e7b05c500f36479c2dad154ae9a4251177
Abdy <abdyrush@gmail.com>
2022-05-12 17:12:16 +0200

    initial things

---
+++ main.c
@@ -0,0 +1,20 @@
+//? Commento singola riga
+/* commento più righe*/
+
+#include <stdio.h> //? libreria principale di C che contiene tutte le funzioni di base
+
+int main() {
+
+
+    //! printf
+    printf("Hello World!"); //? Stampa "Hello World"!
+    printf("Sto imparando C!"); //? Stampa "Sto imparando C" nella stessa riga del printf iniziale, va aggiunto per forza lo \n all'inizo
+    printf("\nnuova riga"); //? Stampa "nuova riga" su una nuova riga
+    //? "\\" per mettere un bacslash      "\n" per mettere una nuova riga     "\"" per mettere una virgoletta     "\t" crea un "tab orizzontale"
+
+    //! variabili
+    // * formattazione: tipo nomeVar = valore;  esempio: int a = 1;
+    
+
+    return 0; //? restituisce 0, perchè il programma è terminato correttamente
+}
