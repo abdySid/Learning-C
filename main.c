bdcb6d2b56c2534fc60ea93d3f099f9f64fe775b
Abdy <abdyrush@gmail.com>
2022-05-12 17:45:12 +0200

    updated main.c

--- main.c
+++ main.c
@@ -12,9 +12,26 @@ int main() {
     printf("\nnuova riga"); //? Stampa "nuova riga" su una nuova riga
     //? "\\" per mettere un bacslash      "\n" per mettere una nuova riga     "\"" per mettere una virgoletta     "\t" crea un "tab orizzontale"
 
+
     //! variabili
     // * formattazione: tipo nomeVar = valore;  esempio: int a = 1;
-    
 
+    int numero = 15; //? crea una variabile di tipo integer chiamata "numero" di cui valore 15
+    numero = 10; //? il valore di "numero" è diventato 10
+
+    //* non è possibile fare questa cosa "printf(numero)" poichè darà un errore
+    //* per stampare il valore di una variabile sarà necessario fare come segue:
+
+    printf("\n\n%d", numero); //? stampa il valore di "numero"
+
+    //? tipi di variabile
+    int varIntera = 10; //? crea una variabile di tipo integer chiamata "varIntera" di cui valore 10
+    float varDecimale = 1.10; //? crea una variabile di tipo float chiamata "varDecimale" di cui valore 1.10
+    char varCarattere = 'M'; //? crea una variabile di tipo char chiamata "varCarattere" di cui valore 'a'
+
+    printf("\n\n%d", varIntera); //? stampa il valore di "varIntera"
+    printf("\n%f", varDecimale); //? stampa il valore di "varDecimale"
+    printf("\n%c", varCarattere); //? stampa il valore di "varCarattere"
+    
     return 0; //? restituisce 0, perchè il programma è terminato correttamente
 }
